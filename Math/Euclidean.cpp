622f040a654bee2d37086a89489b6dbd0e70177f
ishan909 <32139516+ishan909@users.noreply.github.com>
2018-06-11 20:26:50 -0500

    Update Euclidean.cpp

--- Math/Euclidean.cpp
+++ Math/Euclidean.cpp
@@ -2,13 +2,14 @@
 
 // finds the gcd of a and b using the Euclidean Algorithm
 int gcd(int a, int b) {
-    if(b == 0){
+    if(b == 0) {
         return a;
     }
-    return gcd(b,a % b);
+    return gcd(b, a % b);
 }
 
-
 int main() {
+    // testing
+    std::cout << gcd(105, 252) << std::endl;
     std::cout << gcd(105, -252) << std::endl;
 }
